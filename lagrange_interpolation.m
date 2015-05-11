b5461eee43d8aeea4c8b1640bd911d5cae84c5f5
PuAnfeng <paf32@qq.com>
2015-05-11 12:57:03 +0800

    拉格朗日插值函数

--- lagrange_interpolation.m
+++ lagrange_interpolation.m
@@ -9,9 +9,6 @@ n = length(X);  % 获取变量个数
 syms x;         % 为实现多项式计算引入的变量
 l = 1;			% 计算L(x)时使用的中间变量
 L = 0;			% 拉格朗日插值多项式L(x)
-%l = ones(1,n);
-%L = zeros(1,n);
-
 
 for i = 1:n
     for j = 1:i-1
@@ -23,20 +20,6 @@ for i = 1:n
     L = L + l * Y(i);
     l = 1;
 end
-
-
-%{
-for i = 1:n
-    for j = 1:i-1
-        l(j) = (x-X(j))/(X(i)-X(j));
-    end
-    for j = i+1:n 
-        l(j) = (x-X(j))/(X(i)-X(j));
-    end 
-    L(j) = prod(l) * Y(i);
-end
-L = sum(L);
-%}
 
 % 返回结果
 % 把L(x)展开成多项式系数矩阵
